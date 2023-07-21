d6074d15aee008b2ff6d6acef884c1e5b902c7dc
lucassantos22 <lucas.afsantos@hotmail.com>
2023-07-20 23:49:51 -0300

    feat: share faker

---
+++ products/src/bootstrap.js
@@ -0,0 +1,10 @@
+const faker = require('faker');
+
+let products = '';
+
+for (let i = 0; i < 3; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+}
+
+document.querySelector('#dev-products').innerHTML = products;
