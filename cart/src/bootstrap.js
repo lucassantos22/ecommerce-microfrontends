3eda6e98b693aa99565153fc90bdc18ba17579cf
lucassantos22 <lucas.afsantos@hotmail.com>
2023-07-22 19:35:26 -0300

    feat: add mount function to cart bootstrap.js

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,15 @@
 const faker = require('faker');
 
-const message = `<div>You have ${faker.random.number()} items in your cart</div>`
+const mount = (el) => {
+    const message = `<div>You have ${faker.random.number()} items in your cart</div>`
+    el.innerHTML = message
+}
 
-document.querySelector('#dev-cart').innerHTML = message
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-cart-dev-only')
+    if (el) {
+        mount(el)
+    }
+}
+
+export { mount }
